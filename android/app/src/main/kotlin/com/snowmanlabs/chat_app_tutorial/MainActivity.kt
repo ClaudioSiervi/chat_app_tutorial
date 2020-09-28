6f7c517e73b2a59c0b00a1c67b2f8803dd31c8b4
Claudio Siervi <claudio.siervi@gmail.com>
2020-09-28 11:26:33 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/snowmanlabs/chat_app_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.snowmanlabs.chat_app_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// ignore: non_constant_identifier_names
+Widget appBarMain(BuildContext context) {
+  return AppBar(
+    title: Image.asset(
+      "assets/images/logo.png",
+      height: 40,
+    ),
+  );
+}
